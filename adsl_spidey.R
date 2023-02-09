91d10f949e4fa1a777df2089d9aee1fa1b72ba77
nicholas <nicholaswe@gmail.com>
2023-02-09 14:58:47 +0000

    New variables

--- adsl_spidey.R
+++ adsl_spidey.R
@@ -26,9 +26,54 @@ adsl_spec <- metacore %>%  select_dataset('ADSL')
 
 # adsl 
 
-adsl <- dm %>%
-  mutate(AGE=AGE, AGEU=AGEU, ARM=ARM, DTHFL=DTHFL, ETHNIC=ETHNIC,
-         RACE=RACE, RFENDTC=RFENDTC, RFSTDTC=RFSTDTC, SEX=SEX,
-         SITEID=SITEID, STUDYID=STUDYID, SUBJID=SUBJID, USUBJID=USUBJID,
-          TRT01P = ARM, TRT01A = ACTARM)
+adsl.aux <- dm %>%
+            mutate(AGE=AGE, AGEU=AGEU, ARM=ARM, DTHFL=DTHFL, ETHNIC=ETHNIC,
+                  RACE=RACE, RFENDTC=RFENDTC, RFSTDTC=RFSTDTC, SEX=SEX,
+                  SITEID=SITEID, STUDYID=STUDYID, SUBJID=SUBJID, USUBJID=USUBJID,
+                  TRT01P = ARM, TRT01A = ACTARM) %>% 
+            mutate(AGEGR1N = case_when(
+              AGE < 65 ~  1,
+              65 <= AGE & AGE < 80 ~ 2,
+              TRUE ~  3
+            )) %>% 
+            mutate(AGEGR1 = AGEGR1N %>% as.character()) %>% 
+            mutate(ITTFL = case_when(
+              ARMCD != "" ~ "Y",
+              TRUE ~ "N"
+            )) %>% 
+            mutate(RACEN = case_when(
+              RACE == "AMERICAN INDIAN OR ALASKA NATIVE" ~ 1,
+              RACE == "ASIAN" ~ 2,
+              RACE == "BLACK OR AFRICAN AMERICAN" ~ 3,
+              RACE == "NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER" ~ 5,
+              RACE == "WHITE" ~ 6,
+              TRUE ~ NA
+            )) %>% 
+            
+  
+  
+### DRAFT ###
+
+derive_vars_dt(adsl.aux, "RFEN", RFENDTC)
+
+## Difference of dates ##
+
+example <- tribble(
+  ~Dates,
+  "2013-03-18",
+  "2013-03-25",
+  "2013-02-28")
+
+
+example2 <- tribble(
+  ~ Dates,
+  "2013-05-18",
+  "2013-03-29",
+  "2014-02-28")
+
+example$Dates <- example$Dates %>% ymd()
+
+example2$Dates <- example2$Dates %>% ymd()
+
+example-example2
 
