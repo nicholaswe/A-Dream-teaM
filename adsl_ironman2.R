91c0c4979cd16c5625c26f1fc56461b0808d6ea2
nelsonamurciab@gmail.com <murciabn>
2023-02-21 13:08:04 +0000

    OK

--- adsl_ironman2.R
+++ adsl_ironman2.R
@@ -34,7 +34,6 @@ ds <- haven::read_xpt("/cloud/project/sdtm/ds.xpt")
 ex <- haven::read_xpt("/cloud/project/sdtm/ex.xpt")
 qs <- haven::read_xpt("/cloud/project/sdtm/qs.xpt")
 sv <- haven::read_xpt("/cloud/project/sdtm/sv.xpt")
-<<<<<<< HEAD
 lb <- haven::read_xpt("/cloud/project/sdtm/lb.xpt")
 qs <- haven::read_xpt("/cloud/project/sdtm/qs.xpt")
 vs <- haven::read_xpt("/cloud/project/sdtm/vs.xpt")
@@ -50,10 +49,8 @@ ta <- haven::read_xpt("/cloud/project/sdtm/ta.xpt")
 ti <- haven::read_xpt("/cloud/project/sdtm/ti.xpt")
 te <- haven::read_xpt("/cloud/project/sdtm/te.xpt")
 suppds <- haven::read_xpt("/cloud/project/sdtm/suppds.xpt")
+suppdm <- haven::read_xpt("/cloud/project/sdtm/suppdm.xpt")
 
-=======
-  suppdm <- haven::read_xpt("/cloud/project/sdtm/suppdm.xpt")
->>>>>>> 875a4ca5a771967d2d82fa7da24ab8b6adbf766b
 
 # Reading ADSL specifications from specs sheet
 adsl_spec <- readxl::read_xlsx("/cloud/project/metadata/specs.xlsx", sheet = "Variables") %>% 
@@ -94,22 +91,20 @@ MMSETOF <- qs %>%
   distinct(USUBJID)
 
 # 219 RACE (compute DM.RACE)---- 222 Add RFENDTC , 222 RFSTDTC
-<<<<<<< HEAD
 # 229 DM.SUBJID
 # 232 DM.ARM
 # 228 TRT01A=TRT01P, i.e., no difference between actual and randomized treatment in this study.
 dm_vars <- dm %>% 
   select(USUBJID, RACE,RFENDTC,RFSTDTC,SEX,SITEID,STUDYID,ARM) %>%  
-  mutate(term=RACE,
-         SUBJID=USUBJID, # create a new variable named term equal to RACE variable
+  mutate(term=RACE,# create a new variable named term equal to RACE variable
+         SUBJID=USUBJID, # 237  DM.SUBJID
          TRT01P=ARM, 
          TRT01A=TRT01P)
-=======
-  dm_race <- dm %>% 
+
+dm_race <- dm %>% 
   select(USUBJID, RACE,RFENDTC,RFSTDTC) %>%  
   mutate(term=RACE) # create a new variable named term equal to RACE variable
 
->>>>>>> 875a4ca5a771967d2d82fa7da24ab8b6adbf766b
 # 220 RACEN ----
 # Create a lookup table filtering with RACE numeric values
 codelistRace <- codelist %>% 
@@ -118,11 +113,10 @@ codelistRace <- codelist %>%
   filter(name=="RACE")
 
 # Add RACE numeric values to dm_race  
-<<<<<<< HEAD
-dm_vars_ <- dm_vars %>%
-  =======
+
+
+
   dm_racen <- dm_race %>%
-  >>>>>>> 875a4ca5a771967d2d82fa7da24ab8b6adbf766b
 # Join DMRACE with Codelist to get RACEN 
 derive_vars_merged_lookup(
   dataset_add = codelistRace,
@@ -135,7 +129,6 @@ derive_vars_merged_lookup(
   derive_vars_dt(
     new_vars_prefix = "RFEN",
     dtc = RFENDTC
-    <<<<<<< HEAD
   ) %>% 
   select(-term)
 
@@ -147,16 +140,15 @@ sv_visit_3 <- sv %>%
   select(USUBJID,VISITNUM,SVSTDTC) %>% 
   filter(VISITNUM==3) %>% 
   derive_vars_dt( # Create a date variable named RFENDT from character variable SVSTDTC
-    new_vars_prefix = "RFEN",
+    new_vars_prefix = "TRTS",
     dtc = SVSTDTC
-  ) %>% 
-  mutate (SVSTDTC=RFENDT)
+  )
 
 # Left join
-dm_SVSTDTC <- dm_vars_ %>% 
+dm_TRTSDT <- dm_vars_ %>% 
   derive_vars_merged(
     dataset_add = sv_visit_3,
-    new_vars = vars(SVSTDTC),
+    new_vars = vars(TRTSDT),
     by_vars = vars(USUBJID)  
   ) 
 
@@ -164,13 +156,13 @@ dm_SVSTDTC <- dm_vars_ %>%
 # 224 Calculate SAFFL ('Y' if ITTFL='Y' and TRTSDT ne missing. 'N' otherwise) ----
 #Note: make a left join to add SAFFL from Naveen Derivations.
 
-dm_SVSTDTC <-dm_SVSTDTC
+dm_TRTSDT <-dm_TRTSDT
 mutate(
   SAFFL=case_when(
     ITTFL = "Y" & ! is.na(SVSTDTC)~ "Y"  
     TRUE ~ "N"))
 
-# 237  DM.SUBJID
+
 
 # 233 Numeric code for TRT01A which corresponds to the randomized dose ----
 # Create a lookup table filtering with ARMN for get numeric values
@@ -181,7 +173,7 @@ codelistArmN <- codelist %>%
   filter(name=="ARMN")
 
 # Add ARMN numeric values by left join  
-dm_vars_plus_ArmN <- dm_SVSTDTC %>%
+dm_vars_plus_ArmN <- dm_TRTSDT %>%
   derive_vars_merged_lookup(
     dataset_add = codelistArmN,
     new_vars = vars(ARMN),
@@ -191,6 +183,30 @@ dm_vars_plus_ArmN <- dm_SVSTDTC %>%
   ) 
 
 
+# 238 VISIT1DT (SV.SVSTDTC when SV.VISITNUM=1, converted to SAS date)
+
+sv_visit_1 <- sv %>% 
+  select(USUBJID,VISITNUM,SVSTDTC) %>% 
+  filter(VISITNUM==1) %>% 
+  derive_vars_dt( # Create a date variable named RFENDT from character variable SVSTDTC
+    new_vars_prefix = "VISIT1",
+    dtc = SVSTDTC
+  )
+
+# Left join
+dm_vars_plus_visit1dt <- dm_vars_plus_ArmN %>% 
+  derive_vars_merged(
+    dataset_add = sv_visit_1,
+    new_vars = vars(VISIT1DT),
+    by_vars = vars(USUBJID)  
+  ) 
+
+
+
+
+
+
+
 =======
   ) 
 
@@ -198,7 +214,6 @@ dm_racen <- dm_racen %>%
   select(-term) 
 
 
-# 236 TRTSDT (SV.SVSTDTC when SV.VISITNUM=3, converted to SAS date)
 
 
 sv
