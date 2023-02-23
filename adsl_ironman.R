bcb283e55c071b448cb2f22d55b8655a97fc681e
naveenveldanda <enaveenkumar.veldanda@gmail.com>
2023-02-23 21:16:31 +0000

    adding more variables

--- adsl_ironman.R
+++ adsl_ironman.R
@@ -41,31 +41,79 @@ ex <- convert_blanks_to_na(ex)
 
 # Gets a list of AM vars required for derivations 
 # NOTE: Look at the difference between the objects
-dm_vars <- vars(STUDYID, USUBJID, SUBJID, SITEID, ARM, AGE, AGEU, RACE, SEX, ETHNIC, RFSTDTC, RFENDTC)
-dm_vars_v <- c("STUDYID", "USUBJID", "SUBJID", "SITEID", "ARM", "AGE", "AGEU", "RACE", "SEX", "ETHNIC", "RFSTDTC", "RFENDTC")
+dm_vars <- vars(STUDYID, USUBJID, SUBJID, SITEID, ARM, AGE, AGEU, RACE, SEX, ETHNIC, RFSTDTC, RFENDTC, DTHFL)
+dm_vars_v <- c("STUDYID", "USUBJID", "SUBJID", "SITEID", "ARM", "AGE", "AGEU", "RACE", "SEX", "ETHNIC", "RFSTDTC", "RFENDTC", "DTHFL")
 
-
-ds_vars <- vars(STUDYID, USUBJID, DSDECOD)
-ds_vars_v <- c("STUDYID", "USUBJID", "DSDECOD")
 
 # User defined functions -----
 
-func_agegr1 <- function(x) {
+format_agegr1 <- function(var) {
    case_when(
-     x < 65 ~ "1",
-     between(x, 65, 80) ~ "2",
-     x >80 ~ "3"
+     var < 65 ~ "<65",
+     between(var, 65, 80) ~ "65-80",
+     var >80 ~ ">80"
    )  
+}
+
+format_agegr1n <- function(var) {
+  case_when(
+    var < 65 ~ 1,
+    between(var, 65, 80) ~ 2,
+    var >80 ~ 3
+  )  
+}
+
+format_trtn <- function(var) {
+  case_when(
+    var == "Placebo" ~ 0,
+    var == "Xanomeline Low Dose" ~ 54,
+    var == "Xanomeline High Dose" ~ 81
+  )  
+}
+
+format_race <- function(var) {
+  case_when(
+    var == "AMERICAN INDIAN OR ALASKA NATIVE" ~ 1,
+    var == "ASIAN" ~ 2,
+    var == "BLACK OR AFRICAN AMERICAN" ~ 3,
+    var == "NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER" ~ 4,
+    var == "WHITE" ~ 5
+    
+  )  
 }
 
 # deriving AGEGR1, AGEGR1N -----
 
 adsl_agegr <- dm %>% 
+  filter(ARM != "Screen Failure") %>%
   select(all_of(dm_vars_v)) %>%
-  mutate(AGEGR1 = func_agegr1(AGE),
-         AGEGR1N = as.integer(AGEGR1))
+  mutate(AGEGR1 = format_agegr1(AGE),
+         AGEGR1N = format_agegr1n(AGE),
+         .after = AGE)
 
 
+# deriving TRT01P, TRT01A, TRT01PN, TRT01AN  ----
 
 
+adsl_trt <- adsl_agegr %>%
+    mutate(TRT01P = ARM,
+           TRT01PN = format_trtn(TRT01P),
+           TRT01A = TRT01P,
+           TRT01AN = format_trtn(TRT01A),
+           .after = ARM,
+           )
 
+# Deriving RFENDT ------
+
+adsl_rfendt <- derive_vars_dt(
+  adsl_trt, 
+  dtc = RFENDTC,
+  new_vars_prefix = "RFEN"
+)
+
+
+# Deriving RACEN  ------
+
+adsl_racen <- adsl_rfendt %>%
+  mutate(RACEN = format_race(RACE),
+         .after = RACE)
