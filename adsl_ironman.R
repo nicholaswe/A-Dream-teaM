8a72bf756742fa6ffd10b75d42718c6c128dc791
naveenveldanda <enaveenkumar.veldanda@gmail.com>
2023-02-24 21:35:41 +0000

    Added more variable derivations

--- adsl_ironman.R
+++ adsl_ironman.R
@@ -37,10 +37,18 @@ dm <- convert_blanks_to_na(dm)
 ds <- convert_blanks_to_na(ds)
 ex <- convert_blanks_to_na(ex)
 
-# Gets a list of AM vars required for derivations 
-# NOTE: Look at the difference between the objects
+# Gets a list of DM vars required for derivations 
 dm_vars <- vars(STUDYID, USUBJID, SUBJID, SITEID, ARM, AGE, AGEU, RACE, SEX, ETHNIC, RFSTDTC, RFENDTC, DTHFL)
 dm_vars_v <- c("STUDYID", "USUBJID", "SUBJID", "SITEID", "ARM", "AGE", "AGEU", "RACE", "SEX", "ETHNIC", "RFSTDTC", "RFENDTC", "DTHFL")
+
+# Gets a list of EX vars required for derivations 
+ex_vars <- vars(STUDYID, USUBJID, EXSTDTC, EXENDTC, EXSEQ)
+ex_vars_v <- c("STUDYID", "USUBJID", "EXSTDTC", "EXENDTC", "EXSEQ")
+
+
+# Gets a list of DS vars required for derivations 
+ds_vars <- vars(STUDYID, USUBJID, DSTERM, DSDECOD, DSCAT, VISITNUM, VISIT, DSSTDTC)
+ds_vars_v <- c("STUDYID", "USUBJID", "DSTERM", "DSDECOD", "DSCAT", "VISITNUM", "VISIT", "DSSTDTC")
 
 
 # User defined functions -----
@@ -80,6 +88,7 @@ format_race <- function(var) {
   )  
 }
 
+
 # deriving AGEGR1, AGEGR1N -----
 
 adsl_agegr <- dm %>% 
@@ -115,3 +124,107 @@ adsl_rfendt <- derive_vars_dt(
 adsl_racen <- adsl_rfendt %>%
   mutate(RACEN = format_race(RACE),
          .after = RACE)
+
+
+# Deriving ITTFL   ------
+
+adsl_ittfl <- adsl_racen %>%
+  mutate(ITTFL = case_when(
+    ARM != "" ~ 'Y',
+    .default = "N"))
+
+
+# Process DS domain -------------
+
+
+ds_dscat <- ds %>%
+  select(all_of(ds_vars_v)) %>%
+  filter(DSCAT == "DISPOSITION EVENT" & DSDECOD != "SCREEN FAILURE") %>%
+  mutate(
+    DCDECOD = DSDECOD,
+    DCSREAS = case_when( DSTERM == "PROTOCOL ENTRY CRITERIA NOT MET" & DCDECOD =="PROTOCOL VIOLATION" ~ "I/E Not Met",
+                         DCDECOD == "ADVERSE EVENT"  ~ "Adverse Event",
+                         DCDECOD == "STUDY TERMINATED BY SPONSOR"  ~ "Sponsor Decision",
+                         DCDECOD == "DEATH"  ~ "Death",
+                         DCDECOD == "WITHDRAWAL BY SUBJECT"  ~ "Withdrew Consent",
+                         DCDECOD == "PHYSICIAN DECISION"  ~ "Physician Decision",
+                         DCDECOD == "PROTOCOL VIOLATION"  ~ "Protocol Violation",
+                         DCDECOD == "LOST TO FOLLOW-UP"  ~ "Lost to Follow-up",
+                         DCDECOD == "LACK OF EFFICACY"  ~ "Lack of Efficacy"
+    ),
+    EOSSTT = case_when(
+      DCDECOD == "COMPLETED" ~ "COMPLETED",
+      DCDECOD != "COMPLETED" ~ "DISCONTINUED"
+    ),
+    VISNUMEN = case_when(
+      VISITNUM == "13" & DSTERM == "PROTOCOL COMPLETED" ~ 12,
+      VISITNUM != "13" & DSTERM == "PROTOCOl COMPLETED" ~ VISITNUM
+    ),
+    DISCONFL = case_when(
+      DCSREAS != "COMPLETED" & !is.na(DCSREAS) ~ "Y"
+    ),
+    DSRAEFL = case_when(
+      DCSREAS != "Adverse Event" ~ "Y"
+    )) 
+
+
+  
+  
+# test count to see if we have subjects with more than one disposition event
+
+# ds_sub_count <- ds_dscat %>%
+#   select(USUBJID) %>%
+#   group_by(USUBJID)
+# 
+# ds_cnt <- ds_sub_count %>%
+#   summarise(n = n()) %>%
+#   filter(n > 1)
+
+# Derive EXENDTM and EXSTDTM ------------
+
+ex_ext <- ex %>%
+  select(all_of(ex_vars_v)) %>%
+  derive_vars_dtm(
+    dtc = EXENDTC,
+    new_vars_prefix = "EXEN",
+    time_imputation = "last"
+  ) %>%
+  derive_vars_dtm(
+    dtc = EXSTDTC,
+    new_vars_prefix = "EXST"
+  )
+
+# Merge DM and EX and add extra columns  ------------
+
+dm_ex <- adsl_ittfl %>%
+  derive_vars_merged(
+    dataset_add = ex_ext,
+    new_vars = vars(TRTSDTM = EXSTDTM, TRTSTMF = EXSTTMF),
+    order = vars(EXSTDTM, EXSEQ),
+    mode = "first",
+    by_vars = vars(STUDYID,USUBJID)
+  ) %>%
+  derive_vars_merged(
+    dataset_add = ex_ext,
+    new_vars = vars(TRTEDTM = EXENDTM, TRTETMF = EXENTMF),
+    order = vars(EXENDTM, EXSEQ),
+    mode = "last",
+    by_vars = vars(STUDYID,USUBJID)
+  )
+
+# Derive DT using DTM for EX variables ------------
+
+dm_ex_tm <- dm_ex %>%
+  derive_vars_dtm_to_dt(source_vars = vars(TRTSDTM, TRTEDTM))
+
+
+# Derive TRTDUR -----------
+
+dm_ex_trtdur <- dm_ex_tm %>%
+  derive_var_trtdurd()
+
+aest_enddt_na <- ex_ext %>%
+  filter(is.na(EXENDTC))
+
+
+
